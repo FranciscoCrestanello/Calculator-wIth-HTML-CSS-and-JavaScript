f2bada761105f7559b2d15422f49dc76852bf833
Francisco Crestanello <crestanellofrancisco@gmail.com>
2022-02-26 10:34:06 -0300

    bug corrected becausa before the app let put a + befor put a number. now it works fine.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let numberOne, numberTwo, operator, result;
 
 CalculatorFunction = (userValue) => {
+    numberOne = document.getElementById("number1").value;
+
+    
     if(userValue == 'delete') {
         deleteACharacter();
         return;
@@ -20,6 +23,10 @@ CalculatorFunction = (userValue) => {
             document.getElementById("number2").value = numberTwo;
             return;
         }
+    }
+    else if(numberOne == ''){
+        alert(`Please, you need to pick a number first. Try again.`);
+        return;
     }
     else if(userValue == '+' || userValue == '-' || userValue == '/' || userValue == '*'){
         if(document.getElementById("operator").value=='') document.getElementById("operator").value = userValue;
@@ -46,7 +53,8 @@ calculateResult = () => {
         case '/': result = parseInt(numberOne) / parseInt(numberTwo);
             break;
         default: alert(`something has been wrong, please try again.`);
-            break;
+            clearInputs();
+            return;
     }
     alert(`Result: ${result}`);
     clearInputs();
